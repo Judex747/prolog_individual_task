dbb76437a884f7424eacca48d5cd4f2501e5f077
Judex747 <43245420+Judex747@users.noreply.github.com>
2018-10-04 07:10:39 +0300

    Добавьте файлы проекта.

---
+++ indiv/Form1.Designer.cs
@@ -0,0 +1,222 @@
+﻿namespace indiv
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(521, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.fileToolStripMenuItem.Text = "Файл";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(121, 22);
+            this.openToolStripMenuItem.Text = "Открыть";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(121, 385);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(270, 36);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Запустить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(115, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Найденные запросы:";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(15, 54);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(247, 108);
+            this.listBox1.TabIndex = 5;
+            this.listBox1.Click += new System.EventHandler(this.listBox1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(271, 181);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(133, 20);
+            this.textBox1.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 165);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(114, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Параметры запроса:";
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(15, 181);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(247, 121);
+            this.listBox2.TabIndex = 8;
+            this.listBox2.SelectedIndexChanged += new System.EventHandler(this.listBox2_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(268, 165);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(112, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Выберите параметр!";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(410, 181);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(98, 29);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Сохранить";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 315);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(91, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Строка запроса:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Enabled = false;
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox2.Location = new System.Drawing.Point(16, 331);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(492, 48);
+            this.textBox2.TabIndex = 12;
+            // 
+            // listBox3
+            // 
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.Location = new System.Drawing.Point(271, 38);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(169, 121);
+            this.listBox3.TabIndex = 13;
+            this.listBox3.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(521, 429);
+            this.Controls.Add(this.listBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "Индивидуальная работа";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ListBox listBox3;
+    }
+}
+
---
+++ indiv/Form1.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+
+namespace indiv
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string path_to_file;
+        string path_to_swipl = @"C:\Program Files (x86)\swipl\bin";
+        string command = "";
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                //создаем новый процесс, который будет работать с консолью
+                Process pr = new Process();
+                pr.StartInfo.FileName = path_to_swipl+ @"\swipl.exe";
+                pr.StartInfo.Arguments = textBox2.Text;
+                pr.StartInfo.UseShellExecute = false;
+                //отключаем использование оболочки, чтобы можно было читать данные вывода
+                //перенаправляем данные вовода
+                pr.StartInfo.RedirectStandardOutput = true;
+                //задаем кодировку, чтобы читать кириллические символы
+                pr.StartInfo.StandardOutputEncoding = Encoding.GetEncoding(1251);
+                pr.OutputDataReceived += new DataReceivedEventHandler(ConsoleOutputHandler);
+                //запрещаем создавать окно для запускаемой программы                
+                pr.StartInfo.CreateNoWindow = true;
+                pr.Start();
+                pr.BeginOutputReadLine();
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show("Ошибка при запуске!\n" + error.Message, Application.ProductName, MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        private void ConsoleOutputHandler(object sendingProcess,DataReceivedEventArgs recieved)
+        {
+            if (!String.IsNullOrEmpty(recieved.Data))
+            {
+
+                MessageBox.Show(recieved.Data);
+
+            }
+        }
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (!File.Exists(path_to_swipl + @"\swipl-win.exe"))
+            {
+                folderBrowserDialog1.ShowDialog();
+                while(folderBrowserDialog1.SelectedPath!=null)
+                {
+                    folderBrowserDialog1.ShowDialog();
+                }
+                path_to_swipl = folderBrowserDialog1.SelectedPath;
+            }
+            string text_from_file = "";
+            openFileDialog1.Filter = "Prolog files|*.pl";
+            openFileDialog1.ShowDialog();
+            if (openFileDialog1.FileName != null)
+            {
+                if (openFileDialog1.FileName != "")
+                {
+                    path_to_file = openFileDialog1.FileName.ToString();
+                    text_from_file = File.ReadAllText(path_to_file);
+                }
+                for (int i = 0; i <= text_from_file.Length - 1; i++)
+                {
+                    if (text_from_file[i] == ':' && text_from_file[i + 1] == '-')
+                    {
+                        string predicate = "";
+                        int i2 = i - 1;
+                        if (i2 > 0)
+                        {
+                            while (text_from_file[i2] != '.')
+                            {
+                                predicate += text_from_file[i2];
+                                i2--;
+                            }
+                        }
+                        char[] inputarray = predicate.ToCharArray();
+                        Array.Reverse(inputarray);
+                        string predicate_r = new string(inputarray);
+                        predicate_r = predicate_r.Replace((char)13, ' ');
+                        predicate_r = predicate_r.Replace((char)10, ' ');
+                        predicate_r = predicate_r.Trim(' ');
+                        if (predicate_r != "")
+                        {
+                            listBox1.Items.Add(predicate_r);
+                        }
+                    }
+                }
+            }
+        }
+
+        private void listBox1_Click(object sender, EventArgs e)
+        {
+            listBox2.Items.Clear();
+            listBox3.Items.Clear();
+            string claus = listBox1.Items[listBox1.SelectedIndex].ToString();
+            string[] s = claus.Split('(');
+            command = s[0];
+            s[1] = s[1].Trim(')');
+            string[] s1 = s[1].Split(',');
+            listBox2.Items.AddRange(s1);
+            listBox3.Items.AddRange(s1);
+            label2.Text = "";
+            textBox2.Text = " -f " + path_to_file + " -g " + listBox1.Items[listBox1.SelectedIndex] + " -t halt";
+        }
+
+        private void listBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listBox2.SelectedIndex >= 0)
+            {
+                label3.Text = listBox2.SelectedItem.ToString() + "=";
+                textBox1.Enabled = true;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != null)
+            {
+                listBox3.Items[listBox2.SelectedIndex] = textBox1.Text;
+                textBox2.Text = " -f " + path_to_file + " -g " + command + "(";
+                for(int i = 0; i <= listBox3.Items.Count - 1; i++)
+                {
+                    textBox2.Text += listBox3.Items[i] + ",";
+                }
+                textBox2.Text = textBox2.Text.Remove(textBox2.Text.Length - 1, 1);
+                textBox2.Text += ")" +  " -t halt";
+            }
+        }
+    }
+}
